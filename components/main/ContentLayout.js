ed4d7912d96e5745b14866fedb3bf8230a0fa2cd
jinwood-afun <jinwoo@a-fun.co.kr>
2021-11-09 15:01:17 +0900

    margin error solved.

--- components/main/ContentLayout.js
+++ components/main/ContentLayout.js
@@ -44,10 +44,10 @@ function ContentLayout() {
     background-color: #444444;
   `
   const Wrapper = styled.View`
-    margin-top: 30;
-    margin-bottom: 10;
-    margin-left: 30;
-    margin-right: 30;
+    margin-top: 60px;
+    margin-bottom: 10px;
+    margin-left: 30px;
+    margin-right: 30px;
     flex-direction: row;
     justify-content: space-between;
     text-align: center;
@@ -58,43 +58,43 @@ function ContentLayout() {
   `
   const AppTitle = styled.Text`
     color: #fff;
-    font-size: 18;
+    font-size: 18px;
     font-weight: 500;
   `
   const AppSub1 = styled.Text`
     color: tomato;
-    margin-bottom: 10;
-    margin-top: 7;
+    margin-bottom: 10px;
+    margin-top: 7px;
     text-align: center;
     justify-content: center;
     font-weight: 500;
-    font-size: 18;
+    font-size: 18px;
   `
   const AppSub2 = styled.Text`
-    font-size: 18;
+    font-size: 18px;
     font-weight: 500;
     color: #7EC8E3;
-    margin-bottom: 10;
-    margin-top: 7;
+    margin-bottom: 10px;
+    margin-top: 7px;
     text-align: center;
     justify-content: center;
   `
   const AppSub3 = styled.Text`
-    margin-top: 7;
-    font-size: 18;
+    margin-top: 7px;
+    font-size: 18px;
     font-weight: 500;
     text-align: center;
     justify-content: center;
     color: #fff;
-    margin-bottom: 10;
+    margin-bottom: 10px;
   `
   const Card = styled.View`
     background-color: #fff;
     flex: 2;
-    border-top-left-radius: 10;
-    border-top-right-radius: 10;
-    margin-right: 10;
-    margin-left: 10;
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+    margin-right: 10px;
+    margin-left: 10px;
   `
   const AddButton = styled.Button`
     flex: 1;
--- screens/Content.js
+++ screens/Content.js
@@ -1,16 +1,13 @@
-
-import AsyncStorage from "@react-native-async-storage/async-storage"
-import React, { useEffect, useState } from "react"
-import { View, Text, Button } from "react-native"
-import { isLoggedInVar, logUserOut, tokenVar } from "../apollo"
-import styled from "styled-components"
+import React from "react"
 import ContentLayout from "../components/main/ContentLayout"
-import { gql, useMutation, useQuery } from "@apollo/client"
+import { gql, useQuery } from "@apollo/client"
+import {userId} from "../screens/User"
+import { tokenDecodeId } from "../apollo"
 
 // query 연결
 const CONTENT_QUERY = gql`
-  query seeContents($id: Int!) {
-  seeContents(id:$id){
+  query seeContents($userId: Int!) {
+  seeContents(userId:$userId){
     product
     price
     sign
@@ -21,29 +18,20 @@ const CONTENT_QUERY = gql`
   }
 }
 `
+const Content = ({userId}) => {
 
-const Content = () => { 
   const {loading, error, data} = useQuery(CONTENT_QUERY,{
-    variables: {
-      id: 2
-    }
+  
   });
-  
   console.log("===============")
-  console.log(loading);
-  console.log(error);
-  console.log(data);
+  console.log(data) 
+ 
   console.log("===============")
   
-  // let test = ``;
-  // if(seeContents(id)){
-  // test = product.seeContents.map((item, index) => {
-  //   <Text key={index}>{item.id} / {item.product}</Text>
-  // })
-  // }
+  
   return (
     <ContentLayout>
-      {/* {test} */}
+
     </ContentLayout>
   )
 }
